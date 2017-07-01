4b3d1603dec147e97929755930e08ec646a309db
Henrique Ricordi <henriquericordi@outlook.com>
2017-07-01 12:31:52 -0300

    up

--- AplicacaoTransformacoesLineares/AplicacaoTransformacoesLineares/FormAplicar.Designer.cs
+++ AplicacaoTransformacoesLineares/AplicacaoTransformacoesLineares/FormAplicar.Designer.cs
@@ -28,10 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.graficoAplicar = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBoxEspaco = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.graficoAplicar)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,29 +40,42 @@
             // 
             this.graficoAplicar.BackColor = System.Drawing.Color.Transparent;
             this.graficoAplicar.BorderlineColor = System.Drawing.Color.Transparent;
-            chartArea3.Name = "ChartArea1";
-            this.graficoAplicar.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.graficoAplicar.Legends.Add(legend3);
+            chartArea1.Name = "ChartArea1";
+            this.graficoAplicar.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.graficoAplicar.Legends.Add(legend1);
             this.graficoAplicar.Location = new System.Drawing.Point(12, 12);
             this.graficoAplicar.Name = "graficoAplicar";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.graficoAplicar.Series.Add(series3);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.graficoAplicar.Series.Add(series1);
             this.graficoAplicar.Size = new System.Drawing.Size(300, 300);
             this.graficoAplicar.TabIndex = 0;
             this.graficoAplicar.Text = "chart1";
             this.graficoAplicar.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // comboBoxEspaco
+            // 
+            this.comboBoxEspaco.FormattingEnabled = true;
+            this.comboBoxEspaco.Items.AddRange(new object[] {
+            "R",
+            "R²",
+            "R³"});
+            this.comboBoxEspaco.Location = new System.Drawing.Point(327, 86);
+            this.comboBoxEspaco.Name = "comboBoxEspaco";
+            this.comboBoxEspaco.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxEspaco.TabIndex = 1;
             // 
             // FormAplicar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 318);
+            this.Controls.Add(this.comboBoxEspaco);
             this.Controls.Add(this.graficoAplicar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "FormAplicar";
             this.Text = "Aplicação de Transformação Linear";
@@ -73,5 +87,6 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart graficoAplicar;
+        private System.Windows.Forms.ComboBox comboBoxEspaco;
     }
 }
