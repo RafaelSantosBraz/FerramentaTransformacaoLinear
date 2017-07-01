1c12652c475fdc58ecc8ea97fa436b9f78a06b6d
Henrique Ricordi <henriquericordi@outlook.com>
2017-07-01 12:22:42 -0300

    up

--- AplicacaoTransformacoesLineares/AplicacaoTransformacoesLineares/FormAplicar.Designer.cs
+++ AplicacaoTransformacoesLineares/AplicacaoTransformacoesLineares/FormAplicar.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.graficoAplicar = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.graficoAplicar)).BeginInit();
             this.SuspendLayout();
+            // 
+            // graficoAplicar
+            // 
+            this.graficoAplicar.BackColor = System.Drawing.Color.Transparent;
+            this.graficoAplicar.BorderlineColor = System.Drawing.Color.Transparent;
+            chartArea3.Name = "ChartArea1";
+            this.graficoAplicar.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.graficoAplicar.Legends.Add(legend3);
+            this.graficoAplicar.Location = new System.Drawing.Point(12, 12);
+            this.graficoAplicar.Name = "graficoAplicar";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.graficoAplicar.Series.Add(series3);
+            this.graficoAplicar.Size = new System.Drawing.Size(300, 300);
+            this.graficoAplicar.TabIndex = 0;
+            this.graficoAplicar.Text = "chart1";
+            this.graficoAplicar.Click += new System.EventHandler(this.chart1_Click);
             // 
             // FormAplicar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(975, 694);
+            this.ClientSize = new System.Drawing.Size(565, 318);
+            this.Controls.Add(this.graficoAplicar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "FormAplicar";
             this.Text = "Aplicação de Transformação Linear";
+            ((System.ComponentModel.ISupportInitialize)(this.graficoAplicar)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart graficoAplicar;
     }
 }
