93b5c85ae07b67007428a517838641c2cdc185bc
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2017-07-01 23:50:31 -0300

    controle.

--- AplicacaoTransformacoesLineares/AplicacaoTransformacoesLineares/Controle.cs
+++ AplicacaoTransformacoesLineares/AplicacaoTransformacoesLineares/Controle.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -29,8 +31,11 @@ namespace AplicacaoTransformacoesLineares
 
         public Boolean VerificarTransformacaoLinear(String funcao, Vetor u, Vetor v)
         {
+            String fU = funcao;
+            fU.Replace("x", (u.X).ToString());
+            fU.Replace("y", (u.Y).ToString());
+            fU.Replace("z", (u.Z).ToString());
             String[] d1 = funcao.Split('=');
-
             if (d1.Length != 2)
             {
                 return false;
@@ -38,21 +43,39 @@ namespace AplicacaoTransformacoesLineares
             String[] d2 = d1[1].Split(',');
             if (d2.Length == 1)
             {
-                if (d2[0].Contains('^'))
+                String[] d3 = d2[0].Split("+-*/".ToCharArray());
+                for (int c = 0; c < d3.Length; c++)
                 {
-                    
+                    if (d3[c].Contains("^"))
+                    {
+                        String[] d4 = d3[c].Split('^');
+                        String aux = "e " + d4[0] + " " + d4[1];
+                        fU.Replace(d4[0] + "^" + d4[1], Math.Pow((Double.Parse(d4[0])), Double.Parse(d4[1])).ToString());
+                    }
                 }
-            } else if (d2.Length == 2)
+            }
+            else if (d2.Length == 2 || d2.Length == 3)
             {
-
-            } else if (d2.Length == 3)
-            {
-
-            } else
+                for (int i = 0; i < d2.Length; i++)
+                {
+                    String[] d3 = d2[i].Split("+-*/".ToCharArray());
+                    for (int c = 0; c < d3.Length; c++)
+                    {
+                        if (d3[c].Contains("^"))
+                        {
+                            String[] d4 = d3[c].Split('^');
+                            String aux = "e " + d4[0] + " " + d4[1];
+                            fU.Replace(d4[0] + "^" + d4[1], Math.Pow((Double.Parse(d4[0])), Double.Parse(d4[1])).ToString());
+                        }
+                    }
+                }
+            }
+            else
             {
                 return false;
             }
-            return true;
+            MessageBox.Show(fU, "AVISO");
+                        return true;
         }
     }
 }
